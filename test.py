e9e38aa3b954cd953911a880a734ba6186dca38e
FlyTiger-dev <2236082470@qq.com>
2024-05-16 23:26:04 +0800

    test vscode git

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+'''
+Descripttion: 
+version: 
+Author: FeihuXue
+email: xuefeihu@stu.edu.xju.cn
+Date: 2024-05-16 22:21:05
+LastEditors: FeihuXue
+LastEditTime: 2024-05-16 23:25:32
+'''
 # Descripttion:
 # version:
 # Author: FeihuXue
@@ -7,3 +16,4 @@
 # LastEditTime: 5/16/2024 10:21 PM
 
 print('So difficult!')
+print('Too difficult!')
